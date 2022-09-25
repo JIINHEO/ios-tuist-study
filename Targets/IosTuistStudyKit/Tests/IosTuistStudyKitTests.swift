4a5cf2545d22f9f4c668c409078bf78b9105b67c
JIINHEO <gjwldls123@gmail.com>
2022-09-25 11:31:36 +0900

    initail commit

---
+++ Targets/IosTuistStudyKit/Tests/IosTuistStudyKitTests.swift
@@ -0,0 +1,8 @@
+import Foundation
+import XCTest
+
+final class IosTuistStudyKitTests: XCTestCase {
+    func test_example() {
+        XCTAssertEqual("IosTuistStudyKit", "IosTuistStudyKit")
+    }
+}
---
+++ Targets/IosTuistStudyUI/Tests/IosTuistStudyUITests.swift
@@ -0,0 +1,8 @@
+import Foundation
+import XCTest
+
+final class IosTuistStudyUITests: XCTestCase {
+    func test_example() {
+        XCTAssertEqual("IosTuistStudyUI", "IosTuistStudyUI")
+    }
+}
